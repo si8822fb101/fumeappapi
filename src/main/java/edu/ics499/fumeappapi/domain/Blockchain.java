9d1ae264d894307814e23020a38e810fbcd920dd
Marselos Reed <69451943+LaForge-One@users.noreply.github.com>
2022-11-07 21:22:48 -0700

    Update Blockchain.java

--- src/main/java/edu/ics499/fumeappapi/domain/Blockchain.java
+++ src/main/java/edu/ics499/fumeappapi/domain/Blockchain.java
@@ -1,4 +1,86 @@
 package edu.ics499.fumeappapi.domain;
+/**
+ * 
+ */
+package edu.ics499.fume.collections;
 
-public class Blockchain {
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import edu.ics499.fume.entities.Block;
+import edu.ics499.fume.hash.Hash;
+
+
+
+
+
+/**
+ * @author marselos a. reed , qaalib farah, john quinlan, ayden sinn, mohamed mahmoud
+ * 
+ * Blockchain Class - Implemented by using a Merkle/Binary Tree
+ */
+public class BlockChain {
+	
+	public static Block treeGenerator(ArrayList<Block> blocks) throws IOException {
+		ArrayList<Block> leafs = new ArrayList<>();
+		
+		for(int i = 0; i < blocks.size(); i++) leafs.add(new Block());
+		return treeBuilder(leafs);
+	}
+	
+	private static Block treeBuilder(ArrayList<Block> chain) {
+		ArrayList<Block> vertexs = new ArrayList<>();
+		
+		while(chain.size() != 1) {
+			int index = 0, height = chain.size();
+			while(index < height) {
+				Block left = chain.get(index);
+				Block right = null;
+				
+				if((index + 1) < height) {
+					right = chain.get(index + 1);
+				} else {
+					right = new Block(null, null, left.getHash());
+				}
+				
+				String vertexHash = Hash.hashCreation(left.getHash() + right.getHash());
+				vertexs.add(new Block(left, right, vertexHash));
+				index +=2;
+				
+			}
+			chain = vertexs;
+			vertexs = new ArrayList<>();
+		}
+		return chain.get(0);
+	}
+	
+	public static void showBlockchain(Block genesis) {
+		if(genesis == null)return;
+		if((Block.getLeft() == null && Block.getRight() == null)) System.out.println(genesis.getHash());
+		
+		Queue<Block> queue = new LinkedList<>();
+		queue.add(genesis);
+		queue.add(null);
+		
+		while(!queue.isEmpty()) {
+			Block cursor = queue.poll();
+			if(cursor != null) System.out.println(cursor.getHash());
+			else {
+				if(!queue.isEmpty()) queue.add(null);
+			}
+			if(cursor != null && Block.getLeft() != null) queue.add(Block.getLeft());
+			if(cursor != null && Block.getRight() != null) queue.add(Block.getRight());
+		}
+		
+		
+		
+	}
+	
+
 }
+
+
+
