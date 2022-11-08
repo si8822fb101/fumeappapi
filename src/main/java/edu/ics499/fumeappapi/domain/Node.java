402b54beb377b01bc0b1390f965993223bd3787f
Marselos Reed <69451943+LaForge-One@users.noreply.github.com>
2022-11-07 21:27:58 -0700

    Update Node.java

--- src/main/java/edu/ics499/fumeappapi/domain/Node.java
+++ src/main/java/edu/ics499/fumeappapi/domain/Node.java
@@ -1,60 +1,78 @@
 package edu.ics499.fumeappapi.domain;
 
+/**
+ * 
+ */
+package edu.ics499.fume.entities;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * @author marselos a. reed, qaalib farah, john quinlan, ayden sinn, mohamed mahmoud
+ *
+ * NODE - Represents a user/device on the network
+ * 
+ */
 public class Node{
-    private String macAddress, ipAddress;
+	private String macAddress, ipAddress; 
+	
+
+	public Node() throws IOException {
+		this.setMacAddress();
+		this.setIpAddress();
+	}
 
 
-    public Node() throws IOException {
-        this.setMacAddress();
-        this.setIpAddress();
-    }
+	/**
+	 * @return the macAddress
+	 */
+	public String getMacAddress() {
+		return macAddress;
+	}
 
 
-    /**
-     * @return the macAddress
-     */
-    public String getMacAddress() {
-        return macAddress;
-    }
+	/**
+	 * @param macAddress the macAddress to set
+	 * @throws UnknownHostException 
+	 * @throws SocketException 
+	 */
+	public void setMacAddress() throws UnknownHostException, SocketException {
+		InetAddress localHost = InetAddress.getLocalHost();
+		NetworkInterface nic = NetworkInterface.getByInetAddress(localHost);
+		byte[] hardwareAddress = nic.getHardwareAddress();
+		
+		String[] hex = new String[hardwareAddress.length];
+		for(int i = 0; i < hardwareAddress.length; i++) {
+			hex[i] = String.format("%02X", hardwareAddress[i]);
+		}
+		macAddress = String.join("-", hex);
+	}
 
 
-    /**
-     * @param macAddress the macAddress to set
-     * @throws UnknownHostException
-     * @throws SocketException
-     */
-    public void setMacAddress() throws UnknownHostException, SocketException {
-        InetAddress localHost = InetAddress.getLocalHost();
-        NetworkInterface nic = NetworkInterface.getByInetAddress(localHost);
-        byte[] hardwareAddress = nic.getHardwareAddress();
-
-        String[] hex = new String[hardwareAddress.length];
-        for(int i = 0; i < hardwareAddress.length; i++) {
-            hex[i] = String.format("%02X", hardwareAddress[i]);
-        }
-        macAddress = String.join("-", hex);
-    }
+	/**
+	 * @return the ipAddress
+	 */
+	public String getIpAddress() {
+		return ipAddress;
+	}
 
 
-    /**
-     * @return the ipAddress
-     */
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-
-    /**
-     * @param ipAddress the ipAddress to set
-     * @throws IOException
-     */
-    public void setIpAddress() throws IOException {
-        try (final DatagramSocket datagramSocket = new DatagramSocket()) {
-            datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
-            ipAddress = datagramSocket.getLocalAddress().getHostAddress();
-        }
-    }
-
+	/**
+	 * @param ipAddress the ipAddress to set
+	 * @throws IOException 
+	 */
+	public void setIpAddress() throws IOException {
+		try (final DatagramSocket datagramSocket = new DatagramSocket()) {
+			datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
+			ipAddress = datagramSocket.getLocalAddress().getHostAddress();
+		}
+	}
+	
 
 
 }
