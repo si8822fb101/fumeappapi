813848d780f9c7a5a44095055dd0e89cdaa5c042
Marselos Reed <69451943+LaForge-One@users.noreply.github.com>
2022-11-07 21:28:24 -0700

    Add files via upload

---
+++ src/main/java/edu/ics499/fumeappapi/domain/Result.java
@@ -0,0 +1,29 @@
+package edu.ics499.fume.facade;
+
+public class Result extends Transmit {
+	public static final int USER_NAME_UNAVAILABLE = 1;
+	public static final int USER_REGISTRATION_COMPLETED = 2;
+	public static final int USER_ONLINE = 3;
+	public static final int USER_OFFLINE = 4;
+	public static final int NETWORK_ALREADY_EXISTS = 5;
+	public static final int NETWORK_CREATED = 6;
+	public static final int MESSAGE_SENT = 7;
+	public static final int MESSAGE_RECEIVED = 8;
+	public static final int FILE_SENT = 9;
+	public static final int FILE_RECIEVED = 10;
+	public static final int OPERATION_FAILED = 11;
+	
+
+	
+	private int resultCode;
+	
+	
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
+
+}
